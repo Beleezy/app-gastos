6b9a7cefda5700391639c0b1148985f0a7eec41c
Beleezy <58004834+Beleezy@users.noreply.github.com>
2026-04-19 14:29:16 -0500

    feat(ahorros): TabSeisMeses wrapper

---
+++ components/ahorros/TabSeisMeses.vue
@@ -0,0 +1,10 @@
+<template>
+  <AhorrosGraficoAhorroMensual
+    @editar="$emit('editar', $event)"
+    @eliminar="$emit('eliminar', $event)"
+  />
+</template>
+
+<script setup>
+defineEmits(['editar', 'eliminar'])
+</script>
